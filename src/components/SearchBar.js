712f3e582a362e820393475f0faa0cdece76724b
Stephanie Fernandes <steph3001@gmail.com>
2020-01-10 17:25:59 -0300

    Added search functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    searchRef = React.createRef();
+
+    handleSearch = () => {
+        this.props.updateSearch(this.searchRef.current.value);
+    }
+
     render() {
         return (
-            <input type='text' placeholder='Pesquisar...' />
+            <input type='text' placeholder='Pesquisar...' ref={this.searchRef} onChange={this.handleSearch} />
         )
     }
 }
--- src/components/WidgetList.js
+++ src/components/WidgetList.js
@@ -3,9 +3,10 @@ import Widget from './Widget';
 
 class WidgetList extends React.Component {
     render() {
+        const filteredWidgets = Object.keys(this.props.widgetList).filter( widget => this.props.widgetList[widget].name.indexOf(this.props.searchValue) !== -1);
         return (
             <ul>
-                {Object.keys(this.props.widgetList).map( key =>
+                {filteredWidgets.map( key =>
                     <Widget key={key} index={key} name={this.props.widgetList[key].name} details={this.props.widgetList[key]} deleteWidget={this.props.deleteWidget} editWidget={this.props.editWidget} />    
                 )}
             </ul>
