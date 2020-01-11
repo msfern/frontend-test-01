0868332800836ab92bc3aef46f0c7dec9c23c3ef
Stephanie Fernandes <steph3001@gmail.com>
2020-01-11 17:05:36 -0300

    Added logic to open/close modal and update widget

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,44 @@ import React from 'react';
 import WidgetList from './WidgetList';
 import Header from './Header';
 import AddWidget from './AddWidget';
+import GraphModal from './GraphModal';
 import Footer from './Footer';
-import sampleCharts from '../sample-charts';
 
 class App extends React.Component {
   state = {
     widgets: {},
-    search: ''
+    search: '',
+    isModalOpen: false,
+    isBeingEdited : false,
+    widgetBeingEdited: {}
+  }
+  
+  openModalEdit = (widget) => {
+    this.setState({widgetBeingEdited: widget.props});
+    this.setState({isModalOpen: true});
+    this.setState({isBeingEdited: true});
+  }
+  
+  openModalAdd = () => {
+    this.setState({isModalOpen: true});
   }
 
-  addNewWidget= () => {
-    // const widgets = {...this.state.widgets};
-    // widgets[`widget${Date.now()}`] = sampleCharts.chart1;
-    // this.setState({widgets});
-    this.setState({widgets: sampleCharts})
+  closeModal = () => {
+    this.setState({isBeingEdited: false});
+    this.setState({isModalOpen: false});
   }
 
-  editWidget = key => {
-    
+  updateWidget = widget => {
+    const widgets = {...this.state.widgets};
+    const widgetBeingEdited = this.state.widgetBeingEdited;
+    if (this.state.isBeingEdited) {
+      widgets[widgetBeingEdited.index] = widget;
+    } else {
+      widgets[`widget${Date.now()}`] = widget;
+    }
+    this.setState({widgets});
+    this.setState({widgetBeingEdited: {}});
+    this.closeModal();
   }
 
   deleteWidget = key => {
@@ -36,9 +56,10 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header updateSearch={this.updateSearch} />
-        <WidgetList widgetList={this.state.widgets} editWidget={this.editWidget} deleteWidget={this.deleteWidget} searchValue={this.state.search} />
-        <AddWidget addNewWidget={this.addNewWidget} />
+        <WidgetList widgetList={this.state.widgets} deleteWidget={this.deleteWidget} openModalEdit={this.openModalEdit} searchValue={this.state.search} />
+        <AddWidget openModalAdd={this.openModalAdd} />
         <Footer />
+        <GraphModal isModalOpen={this.state.isModalOpen} closeModal={this.closeModal} updateWidget={this.updateWidget} widgetBeingEdited={this.state.widgetBeingEdited} isBeingEdited={this.state.isBeingEdited} />
       </div>
     );
   }
