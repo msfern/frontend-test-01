3c339aaaa6a6dbf2af13ffb42f91778cd5555681
Stephanie Fernandes <steph3001@gmail.com>
2020-01-10 22:27:13 -0300

    Fixed prop call

--- src/components/WidgetList.js
+++ src/components/WidgetList.js
@@ -3,11 +3,13 @@ import Widget from './Widget';
 
 class WidgetList extends React.Component {
     render() {
-        const filteredWidgets = Object.keys(this.props.widgetList).filter( widget => this.props.widgetList[widget].title.text.indexOf(this.props.searchValue) !== -1);
+        
+        const filteredWidgets = Object.keys(this.props.widgetList).filter( widget => this.props.widgetList[widget].title.text.toLowerCase().indexOf(this.props.searchValue.toLowerCase()) !== -1);
+
         return (
             <ul className='widget-list'>
                 {filteredWidgets.map( key =>
-                    <Widget key={key} index={key} name={this.props.widgetList[key].name} details={this.props.widgetList[key]} deleteWidget={this.props.deleteWidget} editWidget={this.props.editWidget} />    
+                    <Widget key={key} index={key} name={this.props.widgetList[key].title.text} details={this.props.widgetList[key]} deleteWidget={this.props.deleteWidget} editWidget={this.props.editWidget} />    
                 )}
             </ul>
         )
